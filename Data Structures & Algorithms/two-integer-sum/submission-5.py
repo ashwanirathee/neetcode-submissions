58547af911d1e5375c57c79b1c54e95d09a670ed
Ashwani Rathee <ashwanirathee.work@gmail.com>
2026-04-30 10:54:33 -0700

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            rem = target - nums[i]
+            for j in range(i+1, len(nums)):
+                if nums[j] == rem:
+                    return [i,j]
