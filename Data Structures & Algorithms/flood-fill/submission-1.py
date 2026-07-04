a8722284bd93da14e3bc51b70b8a482fd423c53d
Ashwani Rathee <ashwanirathee.work@gmail.com>
2026-07-03 20:35:37 -0700

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def floodFillHelper(self, image, sr, sc, color, og_color):
+        if sr < 0 or sr >= len(image) or sc < 0 or sc >= len(image[0]):
+            return 
+
+        # print(sr, sc)
+        if image[sr][sc] != og_color:
+            return
+        
+        image[sr][sc] = color
+        self.floodFillHelper(image, sr-1, sc, color, og_color)
+        self.floodFillHelper(image, sr+1, sc, color, og_color)
+        self.floodFillHelper(image, sr, sc-1, color, og_color)
+        self.floodFillHelper(image, sr, sc+1, color, og_color)
+        return image
+
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if image[sr][sc] == color:
+            return image
+        self.floodFillHelper(image, sr, sc, color, image[sr][sc])
+        return image
