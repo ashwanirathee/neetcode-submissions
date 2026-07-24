564327f92af79ad8c8597aa4dd3a923fd6c55b5b
Ashwani Rathee <ashwanirathee.work@gmail.com>
2026-07-24 13:07:12 -0700

    Add: longest-palindromic-substring - submission-6

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-6.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        memo = {}
+        def dfs(left, right):
+            if left > right:
+                return
+
+            pkey = (left, right)
+            current = s[left:right+1]
+            if pkey in memo:
+                return memo[pkey]
+
+            if current == current[::-1]:
+                memo[pkey] = current
+                return current
+
+            rleft = dfs(left+1, right)
+            rright = dfs(left, right-1)
+            if len(rleft) > len(rright):
+                memo[pkey] = rleft
+                return rleft
+            
+            memo[pkey] = rright
+            return rright
+        
+        return dfs(0, len(s)-1)
