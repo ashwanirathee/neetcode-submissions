de6a4fe5f1f371d88d61e132b54b16dcd8cda9a3
Ashwani Rathee <ashwanirathee.work@gmail.com>
2026-07-25 09:13:26 -0700

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+        n = len(digits)
+        for idx in range(n):
+            curr = digits[n-idx-1]
+            res = curr + carry
+            if res >= 10:
+                digits[n-idx-1] = res % 10
+                carry = 1
+            else:
+                digits[n-idx-1] = res
+                carry = 0
+            # print(carry)
+
+        if carry == 1:
+            res = [1]
+            res.extend(digits)
+            digits = res
+        return digits
